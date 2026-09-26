fbb438c9cdf63b2d64f561c21ebe013ee042fcfd
nazofobi-gpt <hsybl90@gmail.com>
2026-09-26 08:24:15 +0200

    G-153 use API23-safe ISO timestamp regex literal

--- app/src/main/java/com/nazofobi/arrivalalarm/RouteStopProgress.kt
+++ app/src/main/java/com/nazofobi/arrivalalarm/RouteStopProgress.kt
@@ -128,5 +128,5 @@ internal fun String?.toTransitEpochSecondsOrNull(): Long? {
 }
 
 private val ISO_TRANSIT_TIMESTAMP = Regex(
-    """^(\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})(\\.\\d+)?(Z|([+-])(\\d{2}):?(\\d{2}))$"""
+    "^(\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})(\\.\\d+)?(Z|([+-])(\\d{2}):?(\\d{2}))$"
 )
